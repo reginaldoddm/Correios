7edfed44fca7f2742b37121275ffde119b220b2b
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-02-02 13:09:53 -0200

    Adicionado padrão de codificação
    Adicionado padrão de codificação PSR

--- Correios.php
+++ Correios.php
@@ -12,77 +12,78 @@ require_once 'CorreiosAbstract/CorreiosRequest.php';
 
 class Correios extends CorreiosRequest
 {
-	
+
     private $codeEmpresa = '';
+
     private $senha = '';
-		
+
     private $cepOrigem;
+
     private $cepDestino;
-		
+
     private $formato = 1;
-		
+
     private $peso;
+
     private $comprimento;
+
     private $altura;
+
     private $largura;
+
     private $diametro;
-	
-	
+
     /**
      * Adicione os CEP'S (origem e destino)
-     * @param string $cepOrigem CEP de Origem
+     * 
+     * @param string $cepOrigem  CEP de Origem
      * @param string $cepDestino CEP de Destino
      * @throws \InvalidArgumentException
      */
     public function __construct($cepOrigem = '', $cepDestino = '')
     {
-    	
         if (empty($cepOrigem) || empty($cepDestino)) {
             throw new \InvalidArgumentException('$cepOrigem e $cepDestino nao podem ser vazios');
         } else {
-    		
+            
             $this->cepOrigem = preg_replace("/[^0-9]/", '', $cepOrigem);
             $this->cepDestino = preg_replace("/[^0-9]/", '', $cepDestino);
-    		
         }
-    	
     }
-	
-	
+
     /**
      * Utilizar esse método se tiver contrato com o correios
+     * 
      * @param string $codeEmpresa número do contrato
      * @param string $senha senha do contrato
      * @return void
      */
     public function autenticacaoContrato($codeEmpresa = '', $senha = '')
     {
-    	
         $this->codeEmpresa = $codeEmpresa;
         $this->senha = $senha;
-    
     }
-	
-	
+
     /**
      * Formato da encomenda (incluindo embalagem) adicione um valor inteiro
-     * @param int  $formato 1 = Formato caixa/pacote, 2 = Formato rolo/prisma, 3 = Envelope
+     * 
+     * @param int $formato  1 = Formato caixa/pacote, 2 = Formato rolo/prisma, 3 = Envelope
      * @return void
      */
     public function setFormato($formato = 1)
     {
-        $this->formato = (int)$formato;
+        $this->formato = (int) $formato;
     }
-	
-	
+
     /**
      * Adicione peso e medidos da encomenda, se os valores forem menor que
      * o padrão dos correios o método adiciona automaticamento o valor mínimo
      * requerido pelo correios
-     * @param mixed int | double $peso
-     * @param int $altura
-     * @param int $largura
-     * @param int $comprimento
+     * 
+     * @param  mixed int | double $peso
+     * @param int $altura            
+     * @param int $largura            
+     * @param int $comprimento            
      * @return void
      */
     public function setPesoMedidas($peso = 0.300, $altura = 2, $largura = 11, $comprimento = 16)
@@ -94,41 +95,38 @@ class Correios extends CorreiosRequest
         
         $this->diametro = $this->altura + $this->largura;
     }
-	
-	
+
     /**
-     * <b>Método que realiza consulta</b> deve ser chamado após setar todos
+     * Método que realiza consulta deve ser chamado após setar todos
      * os parametros
+     * 
      * @return boolean
      */
     public function consulta()
     {
-    	
         $params = array(
-            'nCdEmpresa'          => $this->codeEmpresa,
-            'sDsSenha'            => $this->senha,
-            'sCepOrigem'          => $this->cepOrigem,
-            'sCepDestino'         => $this->cepDestino,
-            'nVlPeso'             => $this->peso,
-            'nCdFormato'          => $this->formato,
-            'nVlComprimento'      => $this->comprimento,
-            'nVlAltura'           => $this->altura,
-            'nVlLargura'          => $this->largura,
-            'nVlDiametro'         => $this->diametro,
-            'sCdMaoPropria'       => $this->maoPropria,
-            'nVlValorDeclarado'   => $this->valorDeclarado,
-            'sCdAvisoRecebimento' => $this->avisoRecebimento,
+            'nCdEmpresa' => $this->codeEmpresa,
+            'sDsSenha' => $this->senha,
+            'sCepOrigem' => $this->cepOrigem,
+            'sCepDestino' => $this->cepDestino,
+            'nVlPeso' => $this->peso,
+            'nCdFormato' => $this->formato,
+            'nVlComprimento' => $this->comprimento,
+            'nVlAltura' => $this->altura,
+            'nVlLargura' => $this->largura,
+            'nVlDiametro' => $this->diametro,
+            'sCdMaoPropria' => $this->maoPropria,
+            'nVlValorDeclarado' => $this->valorDeclarado,
+            'sCdAvisoRecebimento' => $this->avisoRecebimento
         );
-    	
-    	if (count($this->servicoParams) > 1) {
-    	    $params['nCdServico'] = implode(',', $this->servicoParams);
-    	} else {
-    	    $params['nCdServico'] = $this->servicoParams[0];
-    	}
-    
-    	
-        //Submit Request cUrl
+        
+        if (count($this->servicoParams) > 1) {
+            $params['nCdServico'] = implode(',', $this->servicoParams);
+        } else {
+            $params['nCdServico'] = $this->servicoParams[0];
+        }
+        
+        // Submit Request cUrl
         return ($this->request($params)) ? TRUE : FALSE;
-    		
     }
 }
--- CorreiosAbstract/CorreiosServices.php
+++ CorreiosAbstract/CorreiosServices.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Classe abstrata que gerencia os serviços dos correios
  * serviços do tipo PAC, SEDEX, Mão própria, aviso de recebimento, etc
@@ -7,88 +8,88 @@
  * @package Correios
  * @copyright GPL General Public License 
  */
+abstract class CorreiosService
+{
+    
+    // variavél que armazena serviços disponíveis
+    protected $codServico = array(
+        40010 => 'Sedex',
+        40045 => 'Sedex a cobrar',
+        40215 => 'Sedex 10',
+        40290 => 'Sedex hoje',
+        41106 => 'PAC',
+        81019 => 'E-Sedex'
+    );
 
-abstract class CorreiosService{
+    protected $servicoParams = array();
 
-	//variavél que armazena serviços disponíveis
-	protected  $codServico = array(
-			40010 => 'Sedex',
-			40045 => 'Sedex a cobrar',
-			40215 => 'Sedex 10',
-			40290 => 'Sedex hoje',
-			41106 => 'PAC',
-			81019 => 'E-Sedex',
-	);
-	
+    protected $maoPropria = 'N';
 
-	protected $servicoParams = array();
-	protected $maoPropria = 'N';
-	protected $valorDeclarado = 0;
-	protected $avisoRecebimento = 'N';	
-	
-	
-	/**
-	 * Adicione o serviço a ser consultado
-	 * @param mixed[] $servico array('41106') ou com mais valores array('40010', '41106')
-	 * @throws \InvalidArgumentException
-	 * @return void
-	 */
-	public function setServicos($servico = array()){
-	
-		if(!is_array($servico)){
-			throw new \InvalidArgumentException('O método '.__METHOD__.' requer um parametro no formato array');
-		}
-	
-		if(count($servico) <= 0){
-			throw new \InvalidArgumentException('O método '.__METHOD__.' requer um parametro no formato array que não seja vazio');
-		}else{
-				
-			foreach ($servico as $value){
-				if(!array_key_exists($value, $this->codServico)){
-						
-					throw new \InvalidArgumentException('Serviço '.$this->codServico[$value].' '. $value . ' não está disponível');
-						
-				}else{
-					$this->servicoParams[] = $value;
-				}
-	
-			}
-				
-		}
-	}
-	
-	
-	
-	/**
-	 * Método que define a mão própria, se não utiliza o serviço de
-	 * mão própria  do correio não e necessário utilizar este método.
-	 * @param string $string N para não S para sim
-	 * @return void
-	 */
-	public function setMaoPropria($string = 'N'){
-		$this->maoPropria = $string;
-	}
-	
-	
-	/**
-	 * Método que define o valor declarado, se não utiliza o serviço
-	 * do correio não e necessário utilizar este método.
-	 * @param int|double $valor valor
-	 * @return void
-	 */
-	public function setValorDeclarado($valor = 0){
-		$this->valorDeclarado = number_format(str_replace(',','.', $valor), 2, '.', ',');
-	}
-	
-	
-	/**
-	 * Método que define o aviso de recebimento, se não utiliza o serviço
-	 * do correio não e necessário utilizar este método.
-	 * @param string $string N para não S para sim
-	 * @return boolean
-	 */
-	public function setAvisoRecebimento($string = 'N'){
-		$this->avisoRecebimento = $string;
-	}
-	
+    protected $valorDeclarado = 0;
+
+    protected $avisoRecebimento = 'N';
+
+    /**
+     * Adicione o serviço a ser consultado
+     * 
+     * @param mixed[] $servico  array('41106') ou com mais valores array('40010', '41106')
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    public function setServicos($servico = array())
+    {
+        if (! is_array($servico)) {
+            throw new \InvalidArgumentException('O método ' . __METHOD__ . ' requer um parametro no formato array');
+        }
+        
+        if (count($servico) <= 0) {
+            throw new \InvalidArgumentException('O método ' . __METHOD__ . ' requer um parametro no formato array que não seja vazio');
+        } else {
+            
+            foreach ($servico as $value) {
+                if (! array_key_exists($value, $this->codServico)) {
+                    
+                    throw new \InvalidArgumentException('Serviço ' . $this->codServico[$value] . ' ' . $value . ' não está disponível');
+                } else {
+                    $this->servicoParams[] = $value;
+                }
+            }
+        }
+    }
+
+    /**
+     * Método que define a mão própria, se não utiliza o serviço de
+     * mão própria do correio não e necessário utilizar este método.
+     * 
+     * @param string $string N para não S para sim
+     * @return void
+     */
+    public function setMaoPropria($string = 'N')
+    {
+        $this->maoPropria = $string;
+    }
+
+    /**
+     * Método que define o valor declarado, se não utiliza o serviço
+     * do correio não e necessário utilizar este método.
+     * 
+     * @param int|double $valor
+     * @return void
+     */
+    public function setValorDeclarado($valor = 0)
+    {
+        $this->valorDeclarado = number_format(str_replace(',', '.', $valor), 2, '.', ',');
+    }
+
+    /**
+     * Método que define o aviso de recebimento, se não utiliza o serviço
+     * do correio não e necessário utilizar este método.
+     * 
+     * @param string $string N para não S para sim
+     * @return boolean
+     */
+    public function setAvisoRecebimento($string = 'N')
+    {
+        $this->avisoRecebimento = $string;
+    }
 }
