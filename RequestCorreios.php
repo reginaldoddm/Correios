2a456527bbb02de7c1dcabcddc08ac7988554346
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-05-12 20:19:34 -0300

    criado método que envia requisição para calculo de frete

--- RequestCorreios.php
+++ RequestCorreios.php
@@ -5,8 +5,52 @@ namespace Correios;
 
 class RequestCorreios {
 
-    public function __construct(Correios $correios, Package $package)
-    {
+    private $urlCalculateShipping = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo';
 
+    /**
+     * Envia a requisição para o webservice para calculo
+     * @param Correios $correios
+     * @param Package $package
+     * @return bool|mixed
+     */
+    public function calculateShipping(Correios $correios, Package $package)
+    {
+        $params = [
+            'nCdEmpresa'          => $correios->getCodeBusiness(),
+            'sDsSenha'            => $correios->getPassword(),
+            'sCepOrigem'          => $correios->getZipCodeSource(),
+            'sCepDestino'         => $correios->getZipCodeDestination(),
+            'nVlPeso'             => $package->getWeight(),
+            'nCdFormato'          => $package->getFormat(),
+            'nVlComprimento'      => $package->getLength(),
+            'nVlAltura'           => $package->getHeight(),
+            'nVlLargura'          => $package->getWeight(),
+            'nVlDiametro'         => $package->getDiameter(),
+            'sCdMaoPropria'       => $correios->getProperHand(),
+            'nVlValorDeclarado'   => $correios->getDeclaredValue(),
+            'sCdAvisoRecebimento' => $correios->getReceivingNotice()
+        ];
+
+        $url = $this->urlCalculateShipping . '?'.http_build_query($params);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        $result = curl_exec($ch);
+        $httpCodeResponse = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
+        if ($httpCodeResponse != '200') {
+            return false;
+        }
+
+        return $result;
     }
+
+
+
 }
