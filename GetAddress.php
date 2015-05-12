72b8a312620397a107c925646e6aa08e27699639
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-05-12 11:50:53 -0300

    removidos espacos

--- GetAddress.php
+++ GetAddress.php
@@ -4,22 +4,21 @@ namespace Correios;
 
 class GetAddress
 {
+    public function __construct($zipCode)
+    {
+        if (empty($zipCode)) {
+            throw new \InvalidArgumentException('zipCode can not be empty');
+        }
 
-	public function __construct($zipCode)
-	{
-		if (empty($zipCode)) {
-			throw new \InvalidArgumentException('zipCode can not be empty');
-		}
-		
-		$getAddress = new SoapClient(null, 
-			array(
-				'location' => 'http://service.bravoswebdesign.com.br/service.php',
-				'uri' => 'http://service.bravoswebdesign.com.br/',
-				'trace' => 0,
-				'encoding' => 'UTF-8'
-			)
-		);
-
-		return json_decode($getAddress->getAddress(array('cep' => $zipCode)));
-	}
+        $getAddress = new SoapClient(null, 
+            array(
+                'location' => 'http://service.bravoswebdesign.com.br/service.php',
+                'uri' => 'http://service.bravoswebdesign.com.br/',
+                'trace' => 0,
+                'encoding' => 'UTF-8'
+            )
+        );
+        
+        return json_decode($getAddress->getAddress(array('cep' => $zipCode)));
+    }
 }
