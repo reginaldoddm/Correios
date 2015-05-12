a4ca3bf47ca45b5b35b903f35a8fe5d1fa8888f6
Reginaldo Moreira <reginaldoddm@gmail.com>
2015-05-12 12:51:08 -0300

    refatoracao de codigo

--- GetAddress.php
+++ GetAddress.php
@@ -29,7 +29,7 @@ class GetAddress
         
         $response = $getAddress->getAddress(array('cep' => $zipCode));
         
-        $this->response = (! empty($response)) ? $response : '';
+        $this->response = (string)$response;
     }
     
     
